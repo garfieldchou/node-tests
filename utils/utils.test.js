7457012f134bc5b4251e4c57609cf17634f7dbe1
garfield <shaudai0402@hotmail.com>
2017-11-03 11:32:59 +0800

    use expect().toBe() to assert a result

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,11 +1,14 @@
+const expect = require('expect');
+
 const utils = require('./utils');
 
 it('should add two numbers', () => {
 	var res = utils.add(33, 11);
 
-	if (res !== 44) {
-		throw new Error(`Expected 44, but got ${res}`);
-	}
+	expect(res).toBe(44);
+	// if (res !== 44) {
+	// 	throw new Error(`Expected 44, but got ${res}`);
+	// }
 });
 
 it('should sqaure a number', () => {
