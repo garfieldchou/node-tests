089f3b1054d01b38ed9362f3e8910362782c058c
garfield <shaudai0402@hotmail.com>
2017-11-03 11:44:04 +0800

    chain together multiple assertions. e.g., toBeA() 'string', 'number', ...

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -5,16 +5,11 @@ const utils = require('./utils');
 it('should add two numbers', () => {
 	var res = utils.add(33, 11);
 
-	expect(res).toBe(44);
-	// if (res !== 44) {
-	// 	throw new Error(`Expected 44, but got ${res}`);
-	// }
+	expect(res).toBe(44).toBeA('number');
 });
 
 it('should sqaure a number', () => {
 	var res = utils.square(3);
 
-	if (res !== 9) {
-		throw new Error(`Expected 9, but got ${res}.`);
-	}
+	expect(res).toBe(9).toBeA('number');
 });
