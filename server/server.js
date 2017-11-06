db47c0329fece5ac62c26374b032347686456703
garfield <shaudai0402@hotmail.com>
2017-11-06 14:20:00 +0800

    a new http endpoint for test, see if response returns specific user or not

--- server/server.js
+++ server/server.js
@@ -9,5 +9,20 @@ app.get('/', (req, res) => {
 	});
 });
 
+// GET /users
+// Give users a name prop and age prop
+app.get('/users', (req, res) => {
+	res.send([{
+		name: 'Mike',
+		age: 27
+	}, {
+		name: 'Andrew',
+		age: 25
+	}, {
+		name: 'Jen',
+		age:26
+	}]);
+});
+
 app.listen(3000);
 module.exports.app = app;
