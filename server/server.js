b3aa123291910956479a6cbbdf6007fa397dc715
garfield <shaudai0402@hotmail.com>
2017-11-06 13:58:37 +0800

    use a combination of supertest and expect we can have super flexible test suites for our http endpoints

--- server/server.js
+++ server/server.js
@@ -4,7 +4,8 @@ var app = express();
 
 app.get('/', (req, res) => {
 	res.status(404).send({
-		error: 'Page not found.'
+		error: 'Page not found.',
+		name: 'Todo App v1.0'
 	});
 });
 
--- server/server.test.js
+++ server/server.test.js
@@ -1,4 +1,5 @@
 const request = require('supertest');
+const expect = require('expect');
 
 var app = require('./server').app;
 
@@ -6,8 +7,10 @@ it('should return hello world response', (done) => {
 	request(app)
 	.get('/')
 	.expect(404)
-	.expect({
-		error: 'Page not found.'
+	.expect((res) => {
+		expect(res.body).toInclude({
+			error: 'Page not found.'
+		});
 	})
 	.end(done);
 });
