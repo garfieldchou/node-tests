523ea632b7a49c5c565af727e029afca014305e8
garfield <shaudai0402@hotmail.com>
2017-11-06 11:45:39 +0800

    export app method for test by supertest module

---
+++ server/server.test.js
@@ -0,0 +1,10 @@
+const request = require('supertest');
+
+var app = require('./server').app;
+
+it('should return hello world response', (done) => {
+	request(app)
+	.get('/')
+	.expect('Hello world!')
+	.end(done);
+});
